eeb7b7cd3649dd6f38635cf9e58b7dc138540868
KapitaZzz <agarussky@gmail.com>
2022-03-21 23:14:59 +0300

    1.8. Состояние объекта [#167388]

--- src/main/java/ru/job4j/oop/Battery.java
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -12,7 +12,7 @@ public class Battery {
     }
 
     public void exchange(Battery another) {
-        another.load += this.load;
+        another.load += this.getLoad();
         this.load = 0;
     }
 }
