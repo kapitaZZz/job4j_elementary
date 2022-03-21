9586fb3db17185c1129076d49411e4468d198c9c
KapitaZzz <agarussky@gmail.com>
2022-03-21 20:21:56 +0300

    1.5. Поля объекта [#166083]

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -1,7 +1,25 @@
 package ru.job4j.oop;
 
 public class Cat {
+    private String name;
+    private String food;
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
+    public void show() {
+        System.out.println(this.name + " - " + this.food);
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
     public static void main(String[] args) {
         Cat kitty = new Cat();
+        kitty.eat("kotletka");
+        kitty.giveNick("Gav");
+        kitty.show();
     }
 }
