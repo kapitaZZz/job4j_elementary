aa910489f40f8ef8485c7ef2d8c19f2d252b3438
KapitaZzz <agarussky@gmail.com>
2022-03-29 19:46:46 +0300

    2. final [#334073]

---
+++ src/main/java/ru/job4j/oaa/Airbus.java
@@ -0,0 +1,36 @@
+package ru.job4j.oaa;
+
+public final class Airbus extends Aircraft {
+    private static final int COUNT_ENGINE = 2;
+
+    private String name;
+
+    public Airbus(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void printModel() {
+        System.out.println("Модель самолета: " + name);
+    }
+
+    public void printCountEngine() {
+        String printInfo = "Количество двигателей равно: ";
+        System.out.println(getName().equals("A380") ? printInfo + 4 : printInfo + COUNT_ENGINE);
+    }
+
+    @Override
+    public String toString() {
+        return "Airbus{"
+                + "name='" + name + '\''
+                + '}';
+    }
+}
