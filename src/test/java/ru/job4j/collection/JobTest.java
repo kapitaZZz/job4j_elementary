b63c0438a7008d891ffff55a66cd675aad1a2fca
KapitaZzz <agarussky@gmail.com>
2022-04-02 19:54:04 +0300

    2. Комбинированный компаратор [#10036]

---
+++ src/test/java/ru/job4j/collection/JobTest.java
@@ -0,0 +1,82 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+
+import java.util.Comparator;
+
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.lessThan;
+import static org.junit.Assert.*;
+
+public class JobTest {
+    @Test
+    public void whenComparatorByNameAndPriority() {
+        Comparator<Job> cmpNamePriority = new NameDesc().thenComparing(new PriorityDesc());
+        int rsl = cmpNamePriority.compare(
+                new Job("Impl task", 0),
+                new Job("Fix bug", 1)
+        );
+        assertThat(rsl, lessThan(0));
+    }
+
+    @Test
+    public void whenComparatorByPriorityAndName() {
+        Comparator<Job> cmpNamePriority = new AscName().thenComparing(new PriorityDesc());
+        int rsl = cmpNamePriority.compare(
+                new Job("Impl task", 0),
+                new Job("Fix bug", 1)
+        );
+        assertThat(rsl, greaterThan(0));
+    }
+
+    @Test
+    public void whenComparatorByNameAndPriorityAsc() {
+        Comparator<Job> cmpNamePriority = new AscName().thenComparing(new PriorityAsc());
+        int rsl = cmpNamePriority.compare(
+                new Job("Impl task", 0),
+                new Job("Fix bug", 1)
+        );
+        assertThat(rsl, greaterThan(0));
+    }
+
+    @Test
+    public void testAscName() {
+        Comparator<Job> comparator = new AscName();
+        int res = comparator.compare(
+                new Job("QWERTY", 0),
+                new Job("ZXCVBN", 0)
+        );
+        assertThat(res, lessThan(0));
+    }
+
+    @Test
+    public void testDescName() {
+        Comparator<Job> comparator = new NameDesc();
+        int res = comparator.compare(
+                new Job("QWERTY", 0),
+                new Job("ZXCVBN", 0)
+        );
+        assertThat(res, greaterThan(0));
+    }
+
+    @Test
+    public void testDescPriority() {
+        Comparator<Job> comparator = new PriorityDesc();
+        int res = comparator.compare(
+                new Job("QWERTY", 4),
+                new Job("QWERTY", 1)
+        );
+        assertThat(res, lessThan(0));
+    }
+
+    @Test
+    public void testAscPriority() {
+        Comparator<Job> comparator = new PriorityAsc();
+        int res = comparator.compare(
+                new Job("QWERTY", 4),
+                new Job("QWERTY", 1)
+        );
+        assertThat(res, greaterThan(0));
+    }
+
+}
