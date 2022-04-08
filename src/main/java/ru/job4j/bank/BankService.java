86a6841815e88a6fd73bfce566641351c3c36555
KapitaZzz <agarussky@gmail.com>
2022-04-08 20:52:48 +0300

    6. Тестовое задание из модуля коллекции Lite переделать на Stream API. [#24260]

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -4,15 +4,17 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Stream;
 
 /**
- *  Сервис по работе со счетом пользователя
+ * Сервис по работе со счетом пользователя
  */
 public class BankService {
     private final Map<User, List<Account>> users = new HashMap<>();
 
     /**
      * Добавляет пользователя в систему если его еще нет
+     *
      * @param user пользователь на добавление
      */
     public void addUser(User user) {
@@ -22,8 +24,9 @@ public class BankService {
 
     /**
      * Создание аккаунта пользователю, если такого еще не существует
+     *
      * @param passport номер паспорта - ключ, по которому будет создан аккаунт
-     * @param account аккаунт пользователя в системе
+     * @param account  аккаунт пользователя в системе
      */
     public void addAccount(String passport, Account account) {
         User user = findByPassport(passport);
@@ -37,43 +40,43 @@ public class BankService {
 
     /**
      * Поиск пользователя по ключу - паспорту
+     *
      * @param passport номер паспорта для поиска
      * @return пользователь в системе, если такой существует
      */
     public User findByPassport(String passport) {
-        for (User user : users.keySet()) {
-            if (passport.equals(user.getPassport())) {
-                return user;
-            }
-        }
-        return null;
+        return users.keySet()
+                .stream()
+                .filter(p -> p.getPassport().equals(passport))
+                .findAny().orElse(null);
     }
 
     /**
      * Поиск пользователя и его аккаунта по реквизитам
-     * @param passport номер паспорта - ключ
+     *
+     * @param passport  номер паспорта - ключ
      * @param requisite реквизиты для поиска Аккаунта пользователя
      * @return аккаунт пользователя в системе
      */
     public Account findByRequisite(String passport, String requisite) {
         User user = findByPassport(passport);
         if (user != null) {
-            for (Account ac : users.get(user)) {
-                if (requisite.equals(ac.getRequisite())) {
-                    return ac;
-                }
-            }
+            return users.get(user)
+                    .stream()
+                    .filter(u -> u.getRequisite().equals(requisite))
+                    .findFirst().orElse(null);
         }
         return null;
     }
 
     /**
      * Операция перевода денег между счетами
-     * @param srcPassport номер паспорта(ключ) счета с которого будет перевод
-     * @param srcRequisite реквизиты аккаунта с которого будет списание
-     * @param destPassport номер паспорта(ключ) счета на который будет перевод
+     *
+     * @param srcPassport   номер паспорта(ключ) счета с которого будет перевод
+     * @param srcRequisite  реквизиты аккаунта с которого будет списание
+     * @param destPassport  номер паспорта(ключ) счета на который будет перевод
      * @param destRequisite реквизиты аккаунта на который будет зачисление
-     * @param amount сумма перевода
+     * @param amount        сумма перевода
      * @return TRUE если перевод выполнен / FALSE если перевод не выполнен
      */
     public boolean transferMoney(String srcPassport, String srcRequisite,
